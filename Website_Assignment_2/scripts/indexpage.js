1bbdabd90d8d0db05d530aaf9f14dc65d96190ec
Floris de Gruijter <f.t.degruijter@students.uu.nl>
2025-03-14 17:48:25 +0100

    some text contents

--- Website_Assignment_2/scripts/indexpage.js
+++ Website_Assignment_2/scripts/indexpage.js
@@ -7,41 +7,46 @@ class IndexPage extends li.Page {
 
     init() {
         super.init();
-        this.initSection();
         this.initArticle();
-    }
-    initSection() {
-        const section = document.createElement('section');
-        const sectionTitle = li.createElemWithText('h2',"Section Title");
-        const sectionText = document.createTextNode(`Nam cum essem in Puteolano Hirtiusque noster, 
-            consul designatus, isdem in locis, vir nobis amicissimus et his studiis, 
-            in quibus nos a pueritia viximus, deditus, multum una eramus, maxime nos quidem exquirentes ea consilia, 
-            quae ad pacem et ad concordiam civium pertinerent. Cum enim omnes post interitum Caesaris 
-            novarum perturbationum causae quaeri viderentur iisque esse occurrendum putaremus, 
-            omnis fere nostra in his deliberationibus consumebatur oratio, idque et saepe alias et 
-            quodam liberiore, quam solebat, et magis vacuo ab interventoribus die, cum ad me ille venisset, 
-            primo ea, quae erant cotidiana et quasi legitima nobis, de pace et de otio.`);
-        section.appendChild(sectionTitle);
-        section.appendChild(sectionText);
-        this.main.appendChild(section);
     }
 
     initArticle() {
         const article = document.createElement('article');
-        const articleTitle = li.createElemWithText('h2',"Article Title");
-        const articleText = document.createTextNode(`Quibus actis, Quid ergo? inquit ille, 
-            quoniam oratorias exercitationes non tu quidem, ut spero, reliquisti, 
-            sed certe philosophiam illis anteposuisti, possumne aliquid audire? Tu vero, 
-            inquam, vel audire vel dicere; nec enim, id quod recte existimas, oratoria illa 
-            studia deserui, quibus etiam te incendi, quamquam flagrantissumum acceperam, nec ea, 
-            quae nunc tracto, minuunt, sed augent potius illam facultatem. Nam cum hoc genere philosophiae, 
-            quod nos sequimur, magnam habet orator societatem; subtilitatem enim ab Academia mutuatur et ei 
-            vicissim reddit ubertatem orationis et ornamenta dicendi. Quam ob rem, inquam, quoniam utriusque 
-            studii nostra possessio est, hodie, utro frui malis, optio sit tua. Tum Hirtius: Gratissumum, inquit, 
-            et tuorum omnium simile; nihil enim umquam abnuit meo studio voluntas tua.`);
-        article.appendChild(articleTitle);
+        const articleTitle = li.createElemWithText('h2',"The website");
+        const articleText = document.createTextNode(`On this website you can view student profiles and course descriptions.
+            The Website consists of three pages, namely; the index page that you are currently on, the students page where 
+            you can view student profiles and the courses page where you can view course descriptions.`);
+        article.appendChild(articleTitle); 
         article.appendChild(articleText);
         this.main.appendChild(article);
+
+        const section1 = document.createElement('section');
+        const section1Title = li.createElemWithText('h3',"Students page");
+        const section1Text = document.createTextNode(`On the students page you can view student profiles. 
+            This can be done by loading in json student files. A number of sample student files are contained in the folder .\\students.
+            By clicking on the select file button you can navigate to the file and select it to view its contents in a readable manner.`);
+        section1.appendChild(section1Title);
+        section1.appendChild(section1Text);
+        article.appendChild(section1);
+
+        const section2 = document.createElement('section');
+        const section2Title = li.createElemWithText('h3',"Courses page");
+        const section2Text = document.createTextNode(`On the courses page you can view a list of course summaries.
+             Simmilarly to student profiles you can select a courses json file. An example files has been prepared under .\\courses.
+             By hovering the mouse cursor over course titles more details can be found on each course.`);
+        section2.appendChild(section2Title);
+        section2.appendChild(section2Text);
+        article.appendChild(section2);
+
+        const section3 = document.createElement('section');
+        const section3Title = li.createElemWithText('h3',"Style editor");
+        const section3Text = document.createTextNode(`In the footer of each page a style editor has been added. 
+            You can select what html element you want to through the top dropdown menu. After choosing a element 
+            to edit you can change the fon size into any valid css font size and the text of the selected element will change accordinglly. 
+            For the font color a color picker has been added to modify the colors. A font type selector was also added to allow the changing of fonts.`);
+        section3.appendChild(section3Title);
+        section3.appendChild(section3Text);
+        article.appendChild(section3);
     }
 }
 
